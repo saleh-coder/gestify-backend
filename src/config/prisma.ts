5d9d135fdbf2ae4e9ed2c6f5070ddbb82d11467d
Amal Saleh <saleh-coder@://github.com>
2026-07-11 23:46:03 +0100

    Fix PrismaClient import for production build

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -6,7 +6,7 @@
  */
 
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client/index.js";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
