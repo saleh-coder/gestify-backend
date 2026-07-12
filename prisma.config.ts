432344f787cdd136c730b9351bfeb0374e698ff7
Amal Saleh <saleh-coder@://github.com>
2026-07-12 15:08:09 +0100

    Allow Prisma generation during deployment

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
     // The CLI will use the Direct URL to alter schemas safely
-    url: env("DIRECT_URL"),
+    url: process.env.DIRECT_URL,
   },
   // ✅ ADDED: Configuração de seed obrigatória para o ecossistema do Prisma 7
   migrations: {
